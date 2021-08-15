7cec1a94ce6ae64203313debd5856e9711a9a9ff
SharanPrasath <80667110+SharanPrasath@users.noreply.github.com>
2021-08-15 16:27:14 +0530

    Add files via upload

---
+++ Task2/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+reportWebVitals();
